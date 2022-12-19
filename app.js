338d0c88e3505cfeb6fa7119bcc686f5b2063544
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2022-12-19 23:08:15 +0900

    feat: 로그인 기능 구현

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
 
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb://localhost:27017/middleware_prac", {
@@ -15,6 +16,7 @@ const app = express();
 const router = express.Router();
 
 const User = require('./models/user');
+// 회원가입
 router.post('/users', async (req, res) => {
   const { nickname, email, password, confirmPassword } = req.body;
 
@@ -45,6 +47,28 @@ router.post('/users', async (req, res) => {
   await user.save();
 
   res.status(201).json({});
+});
+
+// 로그인
+router.post('/auth', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  // 1. 사용자가 존재하지 않거나, 
+  // 2. 입력받은 password와 사용자의 password가 다를 때 에러메시지가 발생해야하낟.
+  if (!user || password !== user.password) {
+    res.status(400).json({
+      errorMessage: '사용자가 존재하지 않거나, 사용자의 password와 입력받은 password가 일치하지 않습니다.'
+    });
+    return;
+  }
+
+  // const token = jwt.sign({ userId: user._id });  // userId를 가상화로 만들었기 때문에 사용 가능
+  const token = jwt.sign({ userId: user.userId }, 'SeongHun');  
+
+  res.status(200).json({ token });
+
 })
 
 app.use("/api", express.urlencoded({ extended: false }), router);
